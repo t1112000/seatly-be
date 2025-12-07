0b6b6bbb5e4364f584de811d0ad9597432bbe7bd
esp-tuyen <tuyen.tran@eastplayers.io>
2025-12-07 18:58:59 +0700

    Refactor Sequelize configuration: consolidate development and production settings into a base configuration for improved maintainability.

--- sequelize/config/config.js
+++ sequelize/config/config.js
@@ -1,23 +1,26 @@
 require('dotenv').config();
 const schemaConfig = process.env.SCHEMA_NAME;
 
+const baseConfig = {
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_MASTER_HOST,
+  port: +process.env.DB_PORT || 5432,
+  dialect: 'postgres',
+  schema: schemaConfig,
+  migrationStorageTableSchema: schemaConfig,
+  dialectOptions: process.env.DB_SSL_REQUIRED
+    ? {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      }
+    : undefined,
+};
+
 module.exports = {
-  development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_MASTER_HOST,
-    port: +process.env.DB_PORT || 5432,
-    dialect: 'postgres',
-    schema: schemaConfig,
-    migrationStorageTableSchema: schemaConfig,
-    dialectOptions: process.env.DB_SSL_REQUIRED
-      ? {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false,
-          },
-        }
-      : undefined,
-  },
+  development: baseConfig,
+  production: baseConfig,
 };
