4720d42ca07c5d8e8046becf9bc517e1ae06bd5b
esp-tuyen <tuyen.tran@eastplayers.io>
2025-12-07 19:02:09 +0700

    Update Sequelize configuration: allow fallback to DB_HOST if DB_MASTER_HOST is not set for improved flexibility.

--- sequelize/config/config.js
+++ sequelize/config/config.js
@@ -5,7 +5,7 @@ const baseConfig = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  host: process.env.DB_MASTER_HOST,
+  host: process.env.DB_MASTER_HOST || process.env.DB_HOST,
   port: +process.env.DB_PORT || 5432,
   dialect: 'postgres',
   schema: schemaConfig,
